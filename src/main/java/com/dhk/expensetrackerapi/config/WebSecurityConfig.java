6b5a60606841683190a071039bcac2098e0472fb
kimdh-hi <zbeld123@gmail.com>
2022-03-13 14:00:45 +0900

    spring-security - 인메모리 테스트 사용자 추가 (InMemoryUserDetailsManager)

--- src/main/java/com/dhk/expensetrackerapi/config/WebSecurityConfig.java
+++ src/main/java/com/dhk/expensetrackerapi/config/WebSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.dhk.expensetrackerapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
@@ -22,5 +29,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("user").password("user1234").authorities("user").build();
+        UserDetails admin = User.withUsername("admin").password("admin1234").authorities("admin").build();
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        auth.userDetailsService(userDetailsManager);
+
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("user1234").authorities("user")
+//                .and()
+//                .withUser("admin").password("admin1234").authorities("admin")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
